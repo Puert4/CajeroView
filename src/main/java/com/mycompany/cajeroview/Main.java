84d907b46650e8fbcd19ef33bbad965f05b60448
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-02-18 15:54:19 -0700

    Se añadió Main

--- src/main/java/com/mycompany/cajeroview/Main.java
+++ src/main/java/com/mycompany/cajeroview/Main.java
@@ -5,20 +5,19 @@
 package com.mycompany.cajeroview;
 
 import com.mycompany.cajeropersistencia.conexion.Conexion;
-import com.mycompany.cajeropersistencia.conexion.IConexion;
 
 /**
  *
  * @author TeLesheo
  */
-public class PuebaWacho {
+public class Main {
     
     public static void main (String [] args){
         
-        IConexion conexion = new Conexion();
+        Conexion conexion = new Conexion();
         
-        SignUpForm jeje = new  SignUpForm(conexion);
-        jeje.setVisible(true);
+        PaginaPrincipalForm PPF = new  PaginaPrincipalForm(conexion);
+        PPF.setVisible(true);
     }
     
 }
